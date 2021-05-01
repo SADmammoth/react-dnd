cd50437e0bdf91c834b5dddbb2e3fe23ef4d3033
SADmammoth <lo.ma200018@gmail.com>
2021-05-01 04:02:52 +0300

    fix: minor

--- src/DropArea/helpers/checkIfAccepted.js
+++ src/DropArea/helpers/checkIfAccepted.js
@@ -1,5 +1,5 @@
 export default function checkIfAccepted(dragging, accept) {
-  if (!accept) return;
+  if (!accept) return true;
   return !Object.entries(accept).some(([key, value]) => {
     if (key.startsWith("data-")) {
       return dragging.getAttribute(key) !== value;
